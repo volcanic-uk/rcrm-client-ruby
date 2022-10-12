e0a64001bbaa731e9c3135ad4e7438504c926e8f
Al Palaran <aljade1902@gmail.com>
2022-10-12 13:21:18 +0100

    VOL-2291 removed valid_fields

--- lib/rcrm/types/applicant_custom_field/applicant_custom_field.rb
+++ lib/rcrm/types/applicant_custom_field/applicant_custom_field.rb
@@ -1,14 +1,5 @@
 module RCRM
   class ApplicantCustomField < Selection
-    self.valid_fields = %i(
-      applicantID
-      passport
-    )
-
-    def get_remote_applicant_custom_fields
-      query = '{__type(name: "applicantCustomField"){fields {name}}}'
-      fields = Connection.connection.request(query).body.data.__type.fields
-      fields.map(&:name)
-    end
-  end
+    self.valid_fields = []
+  end 
 end
