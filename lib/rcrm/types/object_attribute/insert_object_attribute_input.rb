f266e60341f5d48dd5cf497d99181b086e32c030
Al Palaran <aljade1902@gmail.com>
2022-11-29 09:35:46 +0000

    VOL-2460 revert object

--- lib/rcrm/types/object_attribute/insert_object_attribute_input.rb
+++ lib/rcrm/types/object_attribute/insert_object_attribute_input.rb
@@ -2,10 +2,14 @@ module RCRM
   class InsertObjectAttributeInput < Input
     valid_fields = %i(
       attributeId
+      attributeMasterId
+      description
       effectiveDate
       expiryDate
       grade
+      gradedDescription
       notes
+      objectAttributeId
       objectId
     )
   end
