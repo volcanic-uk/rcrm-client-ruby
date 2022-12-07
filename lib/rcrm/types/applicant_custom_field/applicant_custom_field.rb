94b7221fc04425194caf059cae59736383797489
Al Palaran <aljade1902@gmail.com>
2022-12-07 14:10:50 +0000

    VOL-2705 revert field search

--- lib/rcrm/types/applicant_custom_field/applicant_custom_field.rb
+++ lib/rcrm/types/applicant_custom_field/applicant_custom_field.rb
@@ -1,7 +1,7 @@
 module RCRM
   class ApplicantCustomField < Selection
     def valid_fields(conn)
-      query = '{__type(name: "updateApplicantCustomField"){name fields{name type{name}}}}'
+      query = '{__type(name: "applicantCustomField"){name fields{name type{name}}}}'
       output = {}
       conn.request(query).body.data.__type.fields.each { |field| output[field.name] = field.type.name }
       output
