1950d30bed9348ca3109e6dc130f99ecc311b9d6
aljadepalaran <38459673+aljadepalaran@users.noreply.github.com>
2024-02-20 11:38:21 +0000

    AB#510411 add update phones (#20)
    * VOL-510411 add update phones
    
    * VOL-510411 update phones filter
    
    * VOL-510411 fix phone filter

--- lib/rcrm/types/phone/phones_filter.rb
+++ lib/rcrm/types/phone/phones_filter.rb
@@ -3,11 +3,15 @@ require 'time'
 module RCRM
   class PhonesFilter < Filter
     def object_id_in(ids)
-      where objectId_in: ids.flatten.compact
+      where objectID_in: ids.flatten.compact
+    end
+
+    def applicant_id_eql(id)
+      where objectID: id
     end
 
     def appicant_id_in(ids)
-      where objectId_in: ids.flatten.compact
+      where objectID_in: ids.flatten.compact
     end
 
     def id_in(*ids)
@@ -16,6 +20,10 @@ module RCRM
 
     def num_equal(num)
       where num: num
+    end
+
+    def communication_type_id_eql(id)
+      where communicationTypeId: id
     end
   end
 end
