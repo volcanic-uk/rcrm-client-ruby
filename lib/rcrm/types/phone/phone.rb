dfba39614f22e5cace566c73d6bb49bc77413245
aljadepalaran <38459673+aljadepalaran@users.noreply.github.com>
2024-03-15 09:12:22 +0000

    add ability to pull phones remotely (#22)
    * add ability to pull phones remotely
    
    * missing parenthesis
    
    * increase version

--- lib/rcrm/types/phone/phone.rb
+++ lib/rcrm/types/phone/phone.rb
@@ -4,5 +4,11 @@ module RCRM
       # objectId is the applicantId
       %i(phoneId objectID num communicationTypeId communicationTypeDescription)
     end
+
+    def available_phones(conn)
+      query = '{communicationTypes{communicationType{description communicationTypeId}}}'
+      fields = conn.request(query).body.data.communicationTypes.communicationType
+      fields.map { |field| ["Phone(#{field.description})", field.communicationTypeId.to_s] }
+    end
   end
 end
